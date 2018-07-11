be15b48b47018d3d10f6d8fdc82ad38bd574a2b0
dbrownidau <the@dbrown.id.au>
2018-07-11 17:40:30 +1000

    Fixed this

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ if ! [ -d "data" ]; then
 echo "data directory does not exist"
 echo "Download from https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/data"
 echo "Structure should look like, EX: ./data/train/cat.1234.jpg"
+mkdir data
 exit
 fi
 python3 ./training.py
